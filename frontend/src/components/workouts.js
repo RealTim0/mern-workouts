019973cee9df7a7e784695fad42505e318026f54
Timothy Mwangi <133085810+RealTim0@users.noreply.github.com>
2023-07-23 18:16:44 +0400

    Update workouts.js

--- frontend/src/components/workouts.js
+++ frontend/src/components/workouts.js
@@ -7,7 +7,7 @@ export default function Details ({workout}){
     const  HandleClick = async () => {
        
 
-        const response = await fetch('https://mern-workouts-backend.vercel.app/workouts/' + workout._id, {
+        const response = await fetch('https://mern-workouts-backend.vercel.app/workouts/' + workout._id,{mode:'no-cors'},{
             method: 'DELETE',
             headers:{
                 'Authorization' : `Bearer ${user.token}`
