ec5fcfd5cfc43ed8e9ec64f954476ea2f24745e4
Timothy Mwangi <133085810+RealTim0@users.noreply.github.com>
2023-07-23 18:15:26 +0400

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('https://mern-workouts-backend.vercel.app/users/signup/', {
+        const response = await fetch('https://mern-workouts-backend.vercel.app/users/signup/',{mode:'no-cors'}, {
             method:'POST',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({email,username,password})  
