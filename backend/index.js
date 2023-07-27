b4110452aad3a072aad6827aa86f990ebd82618c
Timothy Mwangi <133085810+RealTim0@users.noreply.github.com>
2023-07-27 11:46:55 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,8 @@ app.use(cors(
     }
 ))
 app.use(express.json())
-app.use('/workouts', workoutRoutes)
-app.use('/users', userRoutes)
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/users', userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
